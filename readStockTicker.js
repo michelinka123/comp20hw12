6006b5120ed9d7f22d7cf3e1c648f469cbd5adc6
michelinka123 <40838517+michelinka123@users.noreply.github.com>
2020-12-07 21:56:34 -0500

    Update readStockTicker.js

--- readStockTicker.js
+++ readStockTicker.js
@@ -4,7 +4,10 @@ var port = process.env.PORT || 3000;
 console.log("This goes to the console window");
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-   res.write("<h2>Hello World</h2>");
+  var qobj = url.parse(req.url, true).query;
+	var ticker = qobj.ticker;
+	var company = qobj.company;
+   res.write("<h2>company</h2>");
    res.write ("Success!  This app is deployed online");
    res.end();
 }).listen(port);
